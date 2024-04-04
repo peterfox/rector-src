6166b0343051644601216d0ef96f3a64fc59aca9
Peter Fox <peter.fox@peterfox.me>
2024-04-04 23:31:12 +0100

    Working prototype

--- rules/CodingStyle/Rector/Closure/StaticClosureRector.php
+++ rules/CodingStyle/Rector/Closure/StaticClosureRector.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Rector\CodingStyle\Rector\Closure;
 
+use PhpParser\Builder\FunctionLike;
 use PhpParser\Node;
+use PhpParser\Node\Expr\CallLike;
 use PhpParser\Node\Expr\Closure;
+use PHPStan\Reflection\ParameterReflectionWithPhpDocs;
 use Rector\CodingStyle\Guard\StaticGuard;
+use Rector\NodeTypeResolver\PHPStan\ParametersAcceptorSelectorVariantsWrapper;
 use Rector\Rector\AbstractRector;
+use Rector\Reflection\MethodReflectionResolver;
+use Rector\Reflection\ReflectionResolver;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -17,7 +23,8 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 final class StaticClosureRector extends AbstractRector
 {
     public function __construct(
-        private readonly StaticGuard $staticGuard
+        private readonly StaticGuard $staticGuard,
+        private readonly ReflectionResolver $reflectionResolver
     ) {
     }
 
@@ -56,19 +63,60 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [Closure::class];
+        return [Closure::class, Node\Expr\StaticCall::class, Node\Expr\MethodCall::class, Node\Expr\FuncCall::class];
     }
 
     /**
-     * @param Closure $node
+     * @param Closure|Node\Expr\StaticCall|Node\Expr\MethodCall|Node\Expr\FuncCall $node
      */
-    public function refactor(Node $node): ?Node
+    public function refactor(Node $node): Node|int|null
     {
+        if (! $node instanceof Closure) {
+
+            $reflection = $this->reflectionResolver->resolveFunctionLikeReflectionFromCall($node);
+
+            if (! $reflection) {
+                return null;
+            }
+
+            if (! $scope = $node->getAttribute('scope')) {
+                return null;
+            }
+
+            $parametersAcceptor = ParametersAcceptorSelectorVariantsWrapper::select(
+                $reflection,
+                $node,
+                $scope,
+            );
+
+            $parameterIndex = [];
+
+            foreach ($parametersAcceptor->getParameters() as $index => $parameter) {
+                if ($parameter instanceof ParameterReflectionWithPhpDocs && $parameter->getClosureThisType() !== null) {
+                    $parameterIndex[] = $index;
+                }
+            }
+
+            foreach ($node->args as $index => $arg) {
+                if ($arg->value instanceof Closure && in_array($index, $parameterIndex, true)) {
+
+                    $arg->value->setAttribute('cannot-be-static', true);
+                }
+            }
+
+            return null;
+        }
+
+        if ($node->hasAttribute('cannot-be-static')) {
+            return null;
+        }
+
         if (! $this->staticGuard->isLegal($node)) {
             return null;
         }
 
         $node->static = true;
+        
         return $node;
     }
 }
